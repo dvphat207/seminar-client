86843c527244de51fcdb0553b68479a6bde1a7c2
vinhphat_98 <dvphat207@gmail.com>
2020-12-14 18:14:10 +0700

    second commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Seminar Software Testing</h1>
+        <h1>Seminar Software Testing </h1>
         <img src={logo} className="App-logo" alt="logo" />
       </header>
     </div>
